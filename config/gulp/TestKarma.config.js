0e9cc2e48fdbc1ef89763f70f1020162ba6961b8
Jörg Wasmeier <j.wasmeier@faba.de>
2016-10-02 19:56:39 +0200

    INIT

---
+++ config/gulp/TestKarma.config.js
@@ -0,0 +1,18 @@
+module.exports = function (gulp){
+    var gulp = require('gulp');
+    var webpack = require('webpack');
+    var path = require('path');
+
+    var Server = require('karma').Server;
+    gulp.task('testKarma', function(done) {
+        new Server({
+            configFile: __dirname + '/../karma/karma.conf.js'
+        }, done).start();
+    });
+
+    gulp.task('testKarma-tdd', function(done) {
+        new Server({
+            configFile: __dirname + '/../karma/karma-tdd.conf.js'
+        }, done).start();
+    });
+}
